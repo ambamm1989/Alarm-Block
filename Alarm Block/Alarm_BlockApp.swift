388118c9348053bb614c03fe75e0ef30a3314783
Adam Brammer <abrammer89@gmail.com>
2023-05-29 16:37:54 -0400

    Initial Commit

---
+++ Alarm Block/Alarm_BlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alarm_BlockApp.swift
+//  Alarm Block
+//
+//  Created by Adam Brammer on 5/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Alarm_BlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
